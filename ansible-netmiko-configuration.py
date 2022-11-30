9a428589a9d7c32241c750c5d53abc5274f5b383
himanshukumaargit <50648085+himanshukumaargit@users.noreply.github.com>
2022-12-01 03:04:29 +0530

    Add files via upload

--- ansible-netmiko-configuration.py
+++ ansible-netmiko-configuration.py
@@ -1,6 +1,18 @@
 from ansible.module_utils.basic import AnsibleModule
 import logging
 import netmiko
+import difflib
+
+def get_config(connection:netmiko.ConnectHandler):
+    connection.enable()
+    return connection.send_command("show running-config")
+
+def diff_config(cfg_old,cfg_new):
+    return difflib.unified_diff(cfg_old.strip().splitlines(),
+                                cfg_new.strip().splitlines(),
+                                fromfile='old config',
+                                tofile='new config',
+                                lineterm='')
 
 def install_config(module: AnsibleModule):
     args = module.params
@@ -15,13 +27,28 @@ def install_config(module: AnsibleModule):
     result={}
     try:
         with netmiko.ConnectHandler(**device) as connection:
+            original_config = get_config(connection)
             output=connection.send_config_set(config_set,cmd_verify=True,error_pattern="% Invalid input")
-            result['changed'] = True
-            result['stdout']=output
             connection.save_config()
+            result['stdout']=output            
+            result['changed'] = True
+            if args['diff_file']:
+                new_config = get_config(connection)
+                diff = diff_config(original_config, new_config)
+                if diff is not None:
+                    diff_file = module.params['diff_file']
+                    if diff_file is not None:
+                        try:
+                            with open(diff_file, "w") as file_handle:
+                                file_handle.write('\n'.join(diff))
+                        except Exception as err:
+                            result['stderr']= err
+            
+
     except Exception as e:
         result['changed'] = False
         result['stderr']= e
+            
     module.exit_json(**result)            
             
 
@@ -33,7 +60,8 @@ def main():
                     user=dict(required=True),
                     password=dict(required=True),
                     device_type=dict(required=False, default='cisco_ios'),
-                    config_file=dict(required=True),              
+                    config_file=dict(required=True),
+                    diff_file=dict(required=False,default=None),              
         ),
         supports_check_mode=True
     )
